eaba113edde6b01ae2c2961487262fa46050164a
Baptiste Frere <baptiste.frere@etu.unice.fr>
2018-12-07 00:02:52 +0100

    Added 404 error page and redirection to /404 when unknown url

---
+++ src/components/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hello">
+    <h2>Not Found ISSOU!!</h2>
+    <img src="https://www.abc.es/media/play/2017/09/08/risitas-abc-kGSG--620x349@abc.jpg">
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
